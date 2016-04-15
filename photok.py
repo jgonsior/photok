d4b0022de3d63be568d3c880f6048a85fa4c994e
laurentfite <laufi130@student.liu.se>
2016-04-15 11:39:08 +0200

    Small modifications to try out Flask

--- photok.py
+++ photok.py
@@ -3,7 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return 'Hello <a href="laurent">Laurent</a>, Sijmen and Julius!'
+
+@app.route('/laurent')
+def laurent_fun():
+    return 'Hello Laurent only!'
+
+@app.route('/sijmen')
+def sijmen_fun():
     return 'Hello Laurent, Sijmen and Julius!'
+
+@app.route('/julius')
+def julius_fun():
+    return 'Hello Laurent, Sijmen and Julius!'
+
 
 if __name__ == '__main__':
     app.run()
