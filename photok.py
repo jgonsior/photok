b006204164c0a4e9d397e61b0adfe07ee7731314
Julius Gonsior <julius@gaenseri.ch>
2016-04-18 20:34:23 +0200

    - divided code into multiple files -> models directory!

--- photok.py
+++ photok.py
@@ -1,78 +1,40 @@
 import os
 from flask import Flask, render_template_string
-from flask_sqlalchemy import SQLAlchemy
-from flask_user import UserManager, UserMixin, SQLAlchemyAdapter, roles_required
+from flask_user import roles_required, UserManager, UserMixin, SQLAlchemyAdapter
 from wtforms.validators import ValidationError
-import config
+from models import users, db
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+db.init_app(app)
 
-db = SQLAlchemy(app)
-
-
-# some models -> need to be transfered to a different file later one :)
-# Define the Role DataModel
-class Role(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(50), unique=True)
+with app.app_context():
+    # Create all database tables
+    db.create_all()
 
 
-# Define the User data model. Make sure to add flask.ext.user UserMixin !!!
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-
-    # Relationships
-    roles = db.relationship('Role', secondary='user_roles',
-            backref=db.backref('users', lazy='dynamic'))
-
-
-    # User authentication information
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False, server_default='')
-    reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
-
-    # User email information
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    confirmed_at = db.Column(db.DateTime())
-
-    # User information
-    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+    def passwordValidator(form, field):
+        password = field.data
+        if app.config['DEBUG'] == True:
+            limit = 3
+        else:
+            limit = 8
+        if len(password) < limit:
+            raise ValidationError(('Password must have at least 3 characters'))
 
 
+    dbAdapter =  SQLAlchemyAdapter(db, users.User) 
+    userManager = UserManager(dbAdapter, app, password_validator=passwordValidator,)
 
-# Define the UserRoles DataModel
-class UserRoles(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
-
-def passwordValidator(form, field):
-    password = field.data
+    # Create a test user
     if app.config['DEBUG'] == True:
-        limit = 3
-    else:
-        limit = 8
-    if len(password) < limit:
-        raise ValidationError(_('Password must have at least 3 characters'))
-
-# Create all database tables
-db.create_all()
-
-dbAdapter =  SQLAlchemyAdapter(db, User) 
-userManager = UserManager(dbAdapter, app, password_validator=passwordValidator,)
-
-
-# Create a test user
-if app.config['DEBUG'] == True:
-    if not User.query.filter(User.username=='test').first():
-        user1 = User(username='test', email='test@example.com', active=True,
-                password=userManager.hash_password('test'))
-        user1.roles.append(Role(name='admin'))
-        user1.roles.append(Role(name='user'))
-        db.session.add(user1)
-        db.session.commit()
-
+        if not users.User.query.filter(users.User.username=='test').first():
+            user1 = users.User(username='test', email='test@example.com', active=True,
+                    password=userManager.hash_password('test'))
+            user1.roles.append(users.Role(name='admin'))
+            user1.roles.append(users.Role(name='user'))
+            db.session.add(user1)
+            db.session.commit()
 
 
 
