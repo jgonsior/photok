b1aa860b247e5c68da0f156919b6cf038fed1876
laurentfite <laufi130@student.liu.se>
2016-04-18 12:00:42 +0200

    Organised the templates folder according to my understanding of Flask templates structure; Added some bootstrap elements in pages to see the difference between the pages

--- photok.py
+++ photok.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    return render_template('pages/homepage.html')
 
 @app.route('/link')
 def link():
-    return render_template('page.html')
+    return render_template('pages/page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
