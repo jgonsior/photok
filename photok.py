320b481eab3e77d54053efc174c9bea76269c27b
Julius Gonsior <julius@gaenseri.ch>
2016-04-18 13:19:53 +0200

    - flask config is now stored in an extra file, flask.cfg - started copy and pasting code from flask-user tutorial - it seem's to   solve all requirements we've got for our user management - git ignores test database

--- photok.py
+++ photok.py
@@ -1,15 +1,45 @@
-from flask import Flask
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
 app = Flask(__name__)
+app.config.from_pyfile('flask.cfg')
 
 # some configs depending on if we're running the code locally or remotely
 if 'OPENSHIFT_POSTGRESQL_DB_URL' in os.environ:
     app.config['SQLALCHEMY_DATABASE_URI'] =os.environ['OPENSHIFT_MYSQL_DB_URL']
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///tmp/testdb.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///testdb.db"
     app.config['DEBUG'] = True
 
+db = SQLAlchemy(app)
+
+
+# Define the User data model. Make sure to add flask.ext.user UserMixin !!!
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # User authentication information
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
+    
+    # User email information
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
+    
+    # User information
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+# Create all database tables
+db.create_all()
+
+
+db_adapter =  SQLAlchemyAdapter(db, User)    
+userManager = UserManager(db_adapter, app)
 
 
 @app.route('/')
