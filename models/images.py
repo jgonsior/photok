5bca65a5f927de8b5bfa04f735c4a113c92e749c
Julius Gonsior <julius@gaenseri.ch>
2016-04-25 13:08:58 +0200

    - added vote model, now comes the fun part, creating some fake data to   have something in the database

--- models/images.py
+++ models/images.py
@@ -5,8 +5,6 @@ class Image(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    contestId = db.Column(db.Integer(), db.ForeignKey('contest.id'))
-
     uploadedOn = db.Column(db.DateTime(), nullable=False)
 
     title = db.Column(db.String(255), nullable=False)
@@ -19,7 +17,7 @@ class Image(db.Model):
     user = db.relationship('User', backref=db.backref('Image', lazy='dynamic'))
 
     contestId = db.Column(db.Integer, db.ForeignKey('contest.id', ondelete='CASCADE'))
-    contest = db.relationship('Contest', backref=db.backref('Contest', lazy='dynamic'))
+    contest = db.relationship('Contest', backref=db.backref('Image', lazy='dynamic'))
 
 
     def __init__(self, title, prize, user, contest, uploadedOn=None, exifData=None):
