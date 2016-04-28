de8dadb5ed376cdb21860129e240fdb42f899f2c
Sijmen van der Willik <sijmenwillik@hotmail.com>
2016-04-28 20:32:02 +0200

    add front end wtform incl. validations and display of errors

--- photok.py
+++ photok.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask, render_template
+import datetime
+from datetime import date
+from flask import Flask, render_template, request
 from flask_user import roles_required, UserManager, UserMixin, SQLAlchemyAdapter
 from wtforms.validators import ValidationError
 from models import users, db
+from wtforms.fields.html5 import DateField
+from wtforms import Form, BooleanField, StringField, FileField, TextAreaField, validators
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -45,15 +49,40 @@ def browse_contests():
     return render_template('pages/browse.html',active="browse")
 
 
-@app.route('/contest') # /<contestName>') TODO, load actual contest
-def view_contest(): # contestName): TODO, load actual contest
-    return render_template('pages/contest.html',active="")
+@app.route('/contest')  # /<contestName>') TODO, load actual contest
+def view_contest():  # contestName): TODO, load actual contest
+    return render_template('pages/show_contest.html',active="")
+
+
+@app.route('/add')
+def create_contest():
+    return render_template('pages/create_contest.html', active="create_contest", form=CreateContestForm())
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def register():
+    form = CreateContestForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return 'success'
+    return render_template('pages/create_contest.html', active="create_contest", form=form)
 
 
 @app.route('/link')
 @roles_required('admin')
 def link():
     return render_template('pages/page.html',active="page")
+
+
+class CreateContestForm(Form):
+    headline = StringField('Headline', [validators.Length(min=4, max=25)])
+    thumbnail = FileField('Thumbnail')
+    theme = StringField('Theme', [validators.Length(min=4, max=25)])
+    workingTitle = StringField('Working title', [validators.Length(min=4, max=25)])
+    description = TextAreaField('description', [validators.Length(min=0, max=300)])
+    startDate = DateField('Start date', default=date.today)
+    endDate = DateField('End date')
+    onlyJuryVote = BooleanField('Only a jury can vote')
+
 
 if __name__ == '__main__':
     app.run()
