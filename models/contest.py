42bc4c181ab6eb2fab32bad2ce46826592a9ae2d
Julius Gonsior <julius@gaenseri.ch>
2016-05-03 14:38:03 +0200

    - moved API code into Contest Model class (don't know if that is best   practice or not, but it seems to be logical to me)

--- models/contest.py
+++ models/contest.py
@@ -1,5 +1,8 @@
 from models import db
 from datetime import datetime
+from flask_restful import reqparse, Resource
+from sqlalchemy.orm import class_mapper
+import sqlalchemy
 
 class Contest(db.Model):
 
@@ -19,3 +22,74 @@ class Contest(db.Model):
            # if k == "createdDate" and v is None:
            #     v = datetime.utcnow()
             setattr(self, k, v)
+
+
+
+
+# 1. get contests from database
+# 2. define all reqired arguments
+# 3. create new contest
+# 4. save contest to database :)
+
+# converts f.e. datetime objects to strings
+def prepare_dict_for_json(d):
+    del(d['_sa_instance_state'])
+    for k,v in d.iteritems():
+        if isinstance(v, datetime):
+            d[k] = str(v)
+    return d
+
+
+parser = reqparse.RequestParser()
+# accept in a magical way all properties of our model :)
+for prop in class_mapper(Contest).iterate_properties:
+    if isinstance(prop, sqlalchemy.orm.ColumnProperty):
+        parser.add_argument(str(prop).replace("Contest.", ""))
+
+
+class ContestApi(Resource):
+    def get(self, contestId):
+        return prepare_dict_for_json(Contest.query.get(contestId).__dict__)
+
+    def delete(self, contestId):
+        contest = Contest.query.get(contestId)
+        db.session.delete(contest)
+        db.session.commit()
+        return '', 204
+
+
+    # put -> update existing contest
+    def put(self, contestId):
+        # deserialize json :)
+        args = parser.parse_args()
+        #save only the changed attributes
+        for k, v in args.iteritems():
+            if v is not None:
+                # parse datetime back
+                if isinstance(Contest.__table__.c[k].type,
+                        sqlalchemy.sql.sqltypes.DateTime):
+                    v = datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f")
+                db.session.query(Contest).filter_by(id=contestId).update({k:v})
+        db.session.commit()
+        return 201
+
+class ContestListApi(Resource):
+    def get(self):
+        contests = {}
+        for c in Contest.query.all():
+            contest = prepare_dict_for_json(c.__dict__)
+            contests[contest['id']] = contest
+        return contests
+
+    # post -> add new contest
+    def post(self):
+        args = parser.parse_args()
+        for k,v in args.iteritems():
+            if isinstance(Contest.__table__.c[k].type,
+                    sqlalchemy.sql.sqltypes.DateTime):
+                args[k] = datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f")
+        contest =  Contest(args)
+        db.session.add(contest)
+        db.session.commit()
+        return 201
+
--- photok.py
+++ photok.py
@@ -2,13 +2,9 @@ import os
 from flask import Flask, render_template, request
 from flask_user import roles_required, UserManager, UserMixin, SQLAlchemyAdapter
 from flask_restful import reqparse, Resource, Api
-from sqlalchemy.orm import  class_mapper
-from pprint import pprint
-import json
-import sqlalchemy
 from wtforms.validators import ValidationError
 from models import db
-from models.contest import Contest
+from models.contest import Contest, ContestApi, ContestListApi
 from models.vote import Vote
 from models.image import Image
 from models.user import User, Role
@@ -94,75 +90,7 @@ with app.app_context():
         db.session.add(vote3)
         db.session.commit()
 
-
-    # 1. get contests from database
-    # 2. define all reqired arguments
-    # 3. create new contest
-    # 4. save contest to database :)
-
-    # converts f.e. datetime objects to strings
-    def prepare_dict_for_json(d):
-        del(d['_sa_instance_state'])
-        for k,v in d.iteritems():
-            if isinstance(v, datetime):
-                d[k] = str(v)
-        return d
-
-
-    parser = reqparse.RequestParser()
-    # accept in a magical way all properties of our model :)
-    for prop in class_mapper(Contest).iterate_properties:
-        if isinstance(prop, sqlalchemy.orm.ColumnProperty):
-            parser.add_argument(str(prop).replace("Contest.", ""))
-
-
-    class ContestApi(Resource):
-        def get(self, contestId):
-            return prepare_dict_for_json(Contest.query.get(contestId).__dict__)
-
-        def delete(self, contestId):
-            contest = Contest.query.get(contestId)
-            db.session.delete(contest)
-            db.session.commit()
-            return '', 204
-
-
-        # put -> update existing contest
-        def put(self, contestId):
-            # deserialize json :)
-            args = parser.parse_args()
-            #save only the changed attributes
-            for k, v in args.iteritems():
-                if v is not None:
-                    # parse datetime back
-                    if isinstance(Contest.__table__.c[k].type,
-                            sqlalchemy.sql.sqltypes.DateTime):
-                        v = datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f")
-                    db.session.query(Contest).filter_by(id=contestId).update({k:v})
-            db.session.commit()
-            return 201
-
-    class ContestListApi(Resource):
-        def get(self):
-            contests = {}
-            for c in Contest.query.all():
-                contest = prepare_dict_for_json(c.__dict__)
-                contests[contest['id']] = contest
-            return contests
-
-        # post -> add new contest
-        def post(self):
-            args = parser.parse_args()
-            for k,v in args.iteritems():
-                if isinstance(Contest.__table__.c[k].type,
-                        sqlalchemy.sql.sqltypes.DateTime):
-                    args[k] = datetime.strptime(v, "%Y-%m-%d %H:%M:%S.%f")
-            contest =  Contest(args)
-            db.session.add(contest)
-            db.session.commit()
-            return 201
-
-
+    # define REST api entry points
     api.add_resource(ContestApi, '/api/contests/<contestId>')
     api.add_resource(ContestListApi, '/api/contests')
 
